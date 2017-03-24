286f746f23b457fcea7d4020859098b24763315e
oszilloskop <oszilloskop@users.noreply.github.com>
2017-03-24 15:41:40 +0100

    Typo: Raspberry Pi Namen angepasst

--- firmware-release-builder.sh
+++ firmware-release-builder.sh
@@ -10,7 +10,7 @@ echo
 
 
 # Default Werte
-FRB_TARGETS=${FRB_TARGETS:-"ar71xx-generic x86-generic x86-64 x86-xen_domu mpc85xx-generic brcm2708,bcm2708 brcm2708,bcm2709 ar71xx-nand"}
+FRB_TARGETS=${FRB_TARGETS:-"ar71xx-generic x86-generic x86-64 x86-xen_domu mpc85xx-generic brcm2708-bcm2708 brcm2708-bcm2709 ar71xx-nand"}
 FRB_BRANCH=${FRB_BRANCH:-none}
 FRB_VERSION=${FRB_VERSION:-Homebrew}
 FRB_CLEANUP=${FRB_CLEANUP:-1}
